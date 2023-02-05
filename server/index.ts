3da142a9a3ad15d46313c504a37f188962208d67
Binit Rupakheti <crusierponder@protonmail.com>
2023-02-05 15:56:10 +0545

    chore : refactored to better implement and fix bugs related to userId

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,40 @@
 import express from 'express';
+import Message from '../types/Message';
 const app = express();
+import User from '../types/User';
 const http = require('http').Server(app);
 import cors from 'cors';
-import { Server } from 'socket.io';
-const PORT = 8000;
-app.use(cors);
-
-app.get('/api', (req, res) => {
-	res.json({
-		message: 'Hello world',
-	});
-});
-
-const io = new Server({
+import { Socket } from 'socket.io';
+const io = require('socket.io')(http, {
 	cors: {
-		origin: 'http://localhost:4000',
+		origin: 'http://localhost:3000',
 	},
 });
+const PORT = 8000;
+let users: User[] = [];
+app.use(cors);
 
-io.on('connection', (socket) => {
+io.on('connection', (socket: Socket) => {
+	console.log(`${socket.id} just connected`);
+	console.log(users);
 	socket.on('disconnect', () => {
+		users = users.filter((user) => user.socketID !== socket.id);
+		io.emit('newUserResponse', users);
 		console.log('🔥: A user disconnected');
+	});
+
+	socket.on('message', (data: Message) => {
+		console.log('a message was just sent');
+		io.emit('messageResponse', data);
+	});
+
+	socket.on('newUser', (data: User) => {
+		console.log('a user just connected');
+		if (users.filter((i) => data.socketID === i.socketID).length === 0) {
+			users.push(data);
+		}
+		console.log(users);
+		io.emit('newUserResponse', users);
 	});
 });
 
